48d47ad70476973a0c4e4940739d987685c984de
Leo <cacate0129@gmail.com>
2018-01-04 16:50:18 +0800

    clean project

--- build/lib/viznet/__init__.py
+++
@@ -1,8 +0,0 @@
-from .context import *
-from .edgenode import *
-from .brush import *
-from .layerwise import *
-
-from . import theme
-from . import setting
-from . import zoo
--- build/lib/viznet/setup.py
+++
@@ -1,9 +0,0 @@
-'''
-Set up file for matrix product state.
-'''
-
-
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration('viznet', parent_package, top_path)
-    return config
